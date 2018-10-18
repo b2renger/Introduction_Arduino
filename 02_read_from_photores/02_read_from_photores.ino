4b802edea5c5bd47870bdd5743c2f00322de56e4
b2renger <berenger.recoules@gmail.com>
2018-10-18 19:29:16 +0200

    photores

---
+++ 02_read_from_photores/02_read_from_photores.ino
@@ -0,0 +1,12 @@
+// http://wiki.seeedstudio.com/Grove-Sound_Sensor/
+// use the serial plotter
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int value = analogRead(A0);
+  Serial.println(value);
+}
