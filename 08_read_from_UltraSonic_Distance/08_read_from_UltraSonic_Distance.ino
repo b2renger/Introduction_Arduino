932be3524714655b176b2239e307a997065d8460
b2renger <berenger.recoules@gmail.com>
2018-10-19 20:55:45 +0200

    ultrasonic distance

---
+++ 08_read_from_UltraSonic_Distance/08_read_from_UltraSonic_Distance.ino
@@ -0,0 +1,35 @@
+int trigPin = 11;    // Trigger
+int echoPin = 10;    // Echo
+long duration, cm;
+
+void setup() {
+
+  Serial.begin (9600);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  duration = pulseIn(echoPin, HIGH);
+
+  // Convert the time into a distance
+  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  delay(250);
+}
